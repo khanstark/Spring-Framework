79122b02063984836c8c1ec14d19a3a735c1c5d4
kaif khan <khanstark91@gmail.com>
2016-08-27 20:29:04 +0530

    REST components definition

---
+++ Spring-Boot/Person.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Person {
+String id;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getFn() {
+	return fn;
+}
+public void setFn(String fn) {
+	this.fn = fn;
+}
+public String getLn() {
+	return ln;
+}
+public void setLn(String ln) {
+	this.ln = ln;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+String fn,ln;
+int age;
+}
